fd3edd1a1b7cf9457ef5f3feb89d6bd640d98f95
I.Maksat <maksat@Air-Maksat.lan>
2025-01-05 22:21:09 +0600

    Added a method for printing a list of orders

---
+++ src/homework/RestaurantOrders.java
@@ -0,0 +1,59 @@
+package homework;
+
+import homework.domain.Order;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class RestaurantOrders {
+    // Этот блок кода менять нельзя! НАЧАЛО!
+    private List<Order> orders;
+
+    private RestaurantOrders(String fileName) {
+        var filePath = Path.of("data", fileName);
+        Gson gson = new Gson();
+        try {
+            orders = List.of(gson.fromJson(Files.readString(filePath), Order[].class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static RestaurantOrders read(String fileName) {
+        var ro = new RestaurantOrders(fileName);
+        ro.getOrders().forEach(Order::calculateTotal);
+        return ro;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+    // Этот блок кода менять нельзя! КОНЕЦ!
+
+    //----------------------------------------------------------------------
+    //------   Реализация ваших методов должна быть ниже этой линии   ------
+    //----------------------------------------------------------------------
+
+    public void printOrders() {
+        orders.forEach(order -> {
+            System.out.println("Customer: " + order.getCustomer().getFullName());
+            System.out.println("Email: " + order.getCustomer().getEmail());
+            System.out.println("Total: " + order.getTotal());
+            System.out.println("Items:");
+            order.getItems().forEach(item ->
+                    System.out.println("- " + item.getName() + ", Price: " + item.getPrice() + ", Amount: " + item.getAmount())
+            );
+            System.out.println("--------------------------------------------");
+        });
+    }
+
+
+    // Наполните этот класс решением домашнего задания.
+    // Вам необходимо создать все необходимые методы
+    // для решения заданий из домашки :)
+    // вы можете добавлять все необходимые imports
+    //
+}
